d61410a7a932adc0f3fc9565c61bc8a68e5c7950
João Paulo <joaopaulokad@gmail.com>
2020-02-21 12:12:55 -0300

    just menu navigation viwe

--- app/src/main/java/com/example/simplestoremanager/m/MainActivity.kt
+++ app/src/main/java/com/example/simplestoremanager/m/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.simplestoremanager
+package com.example.simplestoremanager.m
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simplestoremanager.R
 
 class MainActivity : AppCompatActivity() {
 
