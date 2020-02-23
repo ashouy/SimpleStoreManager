81dfbcadee5e9b8eddad63ca5d9a220254e7142a
jp <joaopaulokad@gmail.com>
2020-02-22 22:16:29 -0300

    toolbar

--- app/src/main/java/com/example/simplestoremanager/m/MainActivity.kt
+++ app/src/main/java/com/example/simplestoremanager/m/MainActivity.kt
@@ -2,12 +2,40 @@ package com.example.simplestoremanager.m
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.simplestoremanager.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var drawer: DrawerLayout
+    lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tool_bar = tool_bar
+        setSupportActionBar(tool_bar)
+
+        drawer = drawer_layout
+
+        toggle = ActionBarDrawerToggle(
+            this, drawer, tool_bar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+
+        drawer.addDrawerListener(toggle)
+
+        toggle.syncState()
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START)
+        else
+            super.onBackPressed()
     }
 }
