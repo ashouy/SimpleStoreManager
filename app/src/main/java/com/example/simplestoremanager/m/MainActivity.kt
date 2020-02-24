477184066fc825d2119bc2131630f71cf1dc7b52
jp <joaopaulokad@gmail.com>
2020-02-24 17:59:40 -0300

    fragments

--- app/src/main/java/com/example/simplestoremanager/m/MainActivity.kt
+++ app/src/main/java/com/example/simplestoremanager/m/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.simplestoremanager.m
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.simplestoremanager.R
+import com.example.simplestoremanager.m.clients.Client_fragment
+import com.example.simplestoremanager.m.products.Product_fragment
+import com.example.simplestoremanager.m.settings.Settings_fragment
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
     lateinit var drawer: DrawerLayout
     lateinit var toggle: ActionBarDrawerToggle
 
@@ -22,6 +28,9 @@ class MainActivity : AppCompatActivity() {
 
         drawer = drawer_layout
 
+        var nav_view = nav_view
+        nav_view.setNavigationItemSelectedListener(this)
+
         toggle = ActionBarDrawerToggle(
             this, drawer, tool_bar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close
@@ -30,6 +39,13 @@ class MainActivity : AppCompatActivity() {
         drawer.addDrawerListener(toggle)
 
         toggle.syncState()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragmaent_Container,
+                Product_fragment()
+            ).commit()
+            nav_view.setCheckedItem(R.id.products_menu)
+        }
     }
 
     override fun onBackPressed() {
@@ -37,5 +53,31 @@ class MainActivity : AppCompatActivity() {
             drawer.closeDrawer(GravityCompat.START)
         else
             super.onBackPressed()
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when (p0.itemId) {
+            R.id.products_menu -> (
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.fragmaent_Container,
+                        Product_fragment()
+                    ).commit()
+                    )
+            R.id.clients_menu -> (
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.fragmaent_Container,
+                        Client_fragment()
+                    ).commit()
+                    )
+            R.id.setting_menu -> (
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.fragmaent_Container,
+                        Settings_fragment()
+                    ).commit()
+                    )
+
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
---
+++ app/src/main/java/com/example/simplestoremanager/m/settings/Settings_fragment.kt
@@ -0,0 +1,26 @@
+package com.example.simplestoremanager.m.settings
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.simplestoremanager.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Settings_fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+
+}
