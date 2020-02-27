76177824a5ba2c443f2469863a30b365b8b650d0
jp <joaopaulokad@gmail.com>
2020-02-26 21:43:42 -0300

    test list products

--- app/src/main/java/com/example/simplestoremanager/m/clients/Client_fragment.kt
+++ app/src/main/java/com/example/simplestoremanager/m/clients/Client_fragment.kt
@@ -14,6 +14,7 @@ import com.example.simplestoremanager.R
  */
 class Client_fragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/com/example/simplestoremanager/m/products/Product_fragment.kt
+++ app/src/main/java/com/example/simplestoremanager/m/products/Product_fragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.example.simplestoremanager.R
 import kotlinx.android.synthetic.main.fragment_product.*
@@ -18,22 +19,24 @@ import kotlinx.android.synthetic.main.fragment_product.*
 class Product_fragment: Fragment() {
 
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        var product = Product(20,"tijolo",45f)
+        var testList = ArrayList<Product>()
+        testList.add(product)
+
+        product_recycler_view.adapter = ProductAdapter(testList!!)
+        product_recycler_view.layoutManager = LinearLayoutManager(activity)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val product = Product(20,"tijolo",45f)
-        val testList = ArrayList<Product>()
-        testList.add(product)
-
-
-        product_recycler_view.adapter = ProductAdapter(testList)
-        product_recycler_view.layoutManager = LinearLayoutManager(activity)
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_product, container, false)
-
     }
+
 
 
 }
